2b5c17dfffc2a4e7600fe52397f4ca496422d6d2
Shari Kahn <kahn.shari2@gmail.com>
2021-07-08 19:28:26 +0300

    Added flip update to controller and router

--- src/flips/flips.controller.js
+++ src/flips/flips.controller.js
@@ -18,7 +18,17 @@ function bodyHasResultProperty(req, res, next) {
     message: "A 'result' property is required.",
   });
 }
-
+function resultPropertyIsValid(req, res, next) {
+  const { data: { result } = {} } = req.body;
+  const validResult = ["heads", "tails", "edge"];
+  if (validResult.includes(result)) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Value of the 'result' property must be one of ${validResult}. Received: ${result}`,
+  });
+}
 function create(req, res) {
   const { data: { result } = {} } = req.body;
   const newFlip = {
@@ -29,8 +39,41 @@ function create(req, res) {
   counts[result] = counts[result] + 1;
   res.status(201).json({ data: newFlip });
 }
+function flipExists(req, res, next) {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  if (foundFlip) {
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Flip id not found: ${flipId}`,
+  });
+}
+
+function read(req, res) {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  res.json({ data: foundFlip });
+}
+function update(req, res) {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  const originalResult = foundFlip.result;
+  const { data: { result } = {} } = req.body;
+  if (originalResult !== result) {
+    // update the flip
+    foundFlip.result = result;
+    // Adjust the counts
+    counts[originalResult] = counts[originalResult] - 1;
+    counts[result] = counts[result] + 1;
+  }
+  res.json({ data: foundFlip });
+}
 
 module.exports = {
-  create: [bodyHasResultProperty, create],
+  create: [bodyHasResultProperty, resultPropertyIsValid, create],
   list,
+  read: [flipExists, read],
+  update: [flipExists, bodyHasResultProperty, resultPropertyIsValid, update],
 };
--- src/flips/flips.router.js
+++ src/flips/flips.router.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("./flips.controller");
 
 router.route("/").get(controller.list).post(controller.create);
+router.route("/:flipId").get(controller.read).put(controller.update);
 
 module.exports = router;
