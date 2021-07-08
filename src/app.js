e6ff9400257adf439072023360d165ee1d0b2d0e
Shari Kahn <kahn.shari2@gmail.com>
2021-07-08 14:47:58 +0300

    added controller and router files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
+
+const flipsRouter = require("./flips/flips.router");
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+
+const flips = require("./data/flips-data");
+
+const counts = require("./data/counts-data");
+
+app.use(express.json());
+
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+  if (foundCount === undefined) {
+    next({ status: 404, message: `Count id not found: ${countId}` });
+  } else {
+    res.json({ data: foundCount });
+  }
+});
+
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+});
+
+app.use("/flips/:flipId", (req, res, next) => {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  if (foundFlip) {
+    res.json({ data: foundFlip });
+  } else {
+    next({ status: 404, message: `Flip id not found: ${flipId}` });
+  }
+});
+
+app.use("/flips", flipsRouter); // Note: app.use
 
 // Not found handler
 app.use((request, response, next) => {
@@ -9,9 +44,9 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
-
 module.exports = app;
